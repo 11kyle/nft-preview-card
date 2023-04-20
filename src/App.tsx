b0d61122fef29c8dd7929fad2b17bfd8f3b47d58
Kyle Johnson <11kyle.johnson@gmail.com>
2023-04-20 16:50:31 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import NFTPreviewCard from './components/nft-preview-card'
+
+function App() {
+
+  return (
+    <div className='grid place-content-center min-h-screen'>
+      <NFTPreviewCard />
+    </div>
+  )
+}
+
+export default App
